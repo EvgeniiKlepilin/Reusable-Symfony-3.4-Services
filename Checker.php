c289b0d1ebddf552111579c6178f65033cad9107
Evgenii Klepilin <evgenii.klepilin@gmail.com>
2018-12-13 10:21:28 +0600

    Update Checker.php
    Added isEntity method

--- Checker.php
+++ Checker.php
@@ -13,6 +13,40 @@ namespace AppBundle\Utils;
  */
 class Checker
 {    
+    private $acceptableEntities = array(
+        'Post',
+        'Comment',
+        'User'
+    );
+
+    /**
+     * If string is not entity, it returns false
+     * Otherwise, it returns properly formatted entity name
+     * @param string $string
+     * @return bool|string
+     */
+    public function isEntity(string $string, bool $plural = false)
+    {
+        //if the class name is plural, remove 's'
+        if ($plural) {
+            if (substr($string, -3) == 'ies') {
+                $singularString = substr_replace($string, "y", -3);
+            } else {
+                $singularString = substr_replace($string, "", -1);
+            }
+        } else {
+            $singularString = $string;
+        }
+        
+        $entityName = ucfirst(strtolower($singularString));
+        
+        if (in_array($entityName, $this->acceptableEntities)) {            
+            return $entityName;
+        } else {
+            return false;
+        }
+    }
+    
     public function isPositiveInt($string)
     {
         return is_numeric($string) && $string > 0;
